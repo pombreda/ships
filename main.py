27a98d67ef153e5716f978648f79f2bc6f6aef7a
Jean-Louis Fuchs <ganwell@fangorn.ch>
2015-01-01 09:20:55 +0100

    Static pages

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
+""" Main routing for tornado app """
+
 import os
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
- 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello world")
- 
+
+
 def main():
-    application = tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+    """ Initialize tornado IOLoop and webserver """
+    handlers = [
+        (r'/', tornado.web.StaticFileHandler, {'path': 'static/index.html'}),
+        (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'static'}),
+    ]
+    application = tornado.web.Application(handlers)
     http_server = tornado.httpserver.HTTPServer(application)
     port = int(os.environ.get("PORT", 5000))
     http_server.listen(port)
     tornado.ioloop.IOLoop.instance().start()
- 
+
 if __name__ == "__main__":
     main()
