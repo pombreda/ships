e645c499d6f04727b94c415b6fff1a2dbd52d3e3
Jean-Louis Fuchs <ganwell@fangorn.ch>
2015-01-02 09:58:16 +0100

    Ported to momoko

--- initdb.py
+++ initdb.py
@@ -1,15 +1,32 @@
 """ Initialize db schema """
+import psycopg2
+import tornado.ioloop as ioloop
+from ships.sql import db
 
-import sqlalchemy
-import os
+def _done(cursor, error):
+    """ Giving feedback """
+    print("Intialization done: %s " % error)
+    try:
+        print(cursor.fetchall())
+    except psycopg2.ProgrammingError:
+        pass
+    io = ioloop.IOLoop.instance()
+    io.stop()
 
-engine = sqlalchemy.create_engine(os.environ['DATABASE_URL'])
-
-with engine.begin() as conn:
-    conn.execute("""
+def init():
+    """ Helper to call db code after ioloop has started """
+    print("Executing initialization")
+    print(db.dsn)
+    db.execute("""
         DROP SCHEMA public CASCADE;
         CREATE SCHEMA public;
-    """)
+    """, callback=_done)
 
+def main():
+    """ Initialize ioloop """
+    io = ioloop.IOLoop.instance()
+    io.add_callback(init)
+    io.start()
 
-
+if __name__ == "__main__":
+    main()
---
+++ ships/sql.py
@@ -0,0 +1,27 @@
+""" Initialize SQL source """
+
+import os
+import momoko
+import urllib.parse as parse
+import logging as lg
+import getpass
+
+parse.uses_netloc.append("postgres")
+url = parse.urlparse(os.environ["DATABASE_URL"])
+_dsntpl = 'dbname=%(db)s user=%(usr)s password=%(pw)s host=%(hst)s port=%(prt)s'
+user = url.username
+if not user:
+    user = getpass.getuser()
+dsn=_dsntpl % {
+    'db'   : url.path[1:],
+    'usr'  : user,
+    'pw'   : url.password,
+    'hst'  : url.hostname,
+    'prt'  : url.port,
+}
+
+lg.debug("Momoko dsn: %s", dsn)
+db = momoko.Pool(
+    dsn=dsn,
+    size=1
+)
